0b2767d6e0197fc20b3c09dc18734c7d482c8b2a
uday-09 <77871085+uday-09@users.noreply.github.com>
2023-04-12 12:25:47 +0530

    changing token for testing

--- src/api/index.js
+++ src/api/index.js
@@ -7,6 +7,6 @@ import { API_BASE_URL } from "../Utils/constants";
 export const Api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE2YjVmZTNkYWMzNjBlMjI5ZTVhNmIiLCJpYXQiOjE2Nzk1OTc5MTd9.h7zLr6dBRuIo-sz3dubuOgJxuzwYs0DKdYqinMS4DOQ`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDM2NTAzZmUyOTlmZGYwZDkxMGE4ZjgiLCJpYXQiOjE2ODEyODEwODd9.ton1gKB0KVqenu_Ins9_39pxjLM0x9ss0oBE_eoUiiw`,
   },
 });
